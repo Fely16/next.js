f1c9e03bc8811b9e688713dc738e292b399de5b6
Hendrik Liebau <mail@hendrik-liebau.de>
2025-02-19 16:16:55 +0100

    Always use `cacheControl` in result metadata

--- packages/next/src/server/base-server.ts
+++ packages/next/src/server/base-server.ts
@@ -2795,6 +2795,7 @@ export default abstract class Server<
       const { metadata } = result
 
       const {
+        cacheControl,
         headers = {},
         // Add any fetch tags that were on the page to the response headers.
         fetchTags: cacheTags,
@@ -2813,7 +2814,7 @@ export default abstract class Server<
       if (
         isAppPath &&
         isSSG &&
-        metadata.revalidate === 0 &&
+        cacheControl?.revalidate === 0 &&
         !this.renderOpts.dev &&
         !isRoutePPREnabled
       ) {
@@ -2835,11 +2836,6 @@ export default abstract class Server<
 
         throw err
       }
-
-      const cacheControl: CacheControl | undefined =
-        metadata.revalidate !== undefined
-          ? { revalidate: metadata.revalidate }
-          : undefined
 
       // Based on the metadata, we can determine what kind of cache result we
       // should return.
